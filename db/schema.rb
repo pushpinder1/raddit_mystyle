b19e073c3e05967f2c4fb1ec7ad8d3a5b2bd329f
pushpinder1 <pushpinder.karwal@outlook.com>
2015-03-05 11:56:41 +0530

    Add association between Link and user

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150305061237) do
+ActiveRecord::Schema.define(version: 20150305062604) do
 
   create_table "links", force: :cascade do |t|
     t.string   "title"
     t.string   "ur"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "links", ["user_id"], name: "index_links_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
